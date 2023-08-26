6163d161db744668c0e155ba204c08ec08092680
Jxino <jinhoesperas@gmail.com>
2023-08-26 23:48:04 +0900

    코드 정리 및 코멘트 작성

--- static/index.js
+++ static/index.js
@@ -1,8 +1,9 @@
-function $(id) {
+function $(id) { // id를 이용해서 DOM 객체를 return
     document.getElement
     return document.getElementById(id);
 }
 
+// 서버로 ajax request를 보내서 응답을 받아오는 함수
 function sendPostRequest(url, data, callback) {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
@@ -11,23 +12,25 @@ function sendPostRequest(url, data, callback) {
     xhr.onreadystatechange = function () { // event handler
         if (xhr.readyState === 4) { // 정상적으로 값을 받으면
             var response = JSON.parse(xhr.responseText);
-            callback(response);
+            callback(response); // 성공했을 때만 callback 함수를 호출
+        } else {
+            console.log("sendPostRequest fail.");
         }
     };
 
-    var json_str = JSON.stringify(data);
+    var json_str = JSON.stringify(data); // 객체를 json string으로 변환
     xhr.send(json_str); // 서버로 request를 날림
 }
 
+// array를 shuffle함
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1)); // 0부터 i 사이의 난수
         [array[i], array[j]] = [array[j], array[i]]; // 요소 교환
     }
 }
-// 신경쇠약 게임 로직
-// state = hide: 0, show: 1, done: 2
 
+// 게임 상태 관리를 위한 변수 선언
 const HIDE = 0;
 const SHOW = 1;
 const DONE = 2;
@@ -38,21 +41,20 @@ var cards = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
 var scores = [0, 0];
 var player = USER;
 
-function initialize() {
+function initialize() { // 게임 상태 초기화
     states = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
     cards = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
     shuffleArray(cards);
-        // player = 사용자: 0, 기계: 1
     scores = [0, 0];
     player = USER;
     for (var i = 0; i < cards.length; i++) {
-        $(i + "").src = "X.png";
+        $(i + "").src = "X.png"; // 닫힌 카드
     }
     $("user_score").innerText = "0";
     $("machine_score").innerText = "0";
 }
 
-function check_finished() {
+function check_finished() { // 게임 종료 조건 체크
     console.log("Check finished");
     var DONE_count = 0;
     for (var i = 0; i < states.length; i++) {
@@ -66,7 +68,7 @@ function check_finished() {
     return false;
 }
 
-function machine_second_card() {
+function machine_second_card() { // 기계가 두 번째 카드 열기
     console.log("Machine second card");
     // DONE이 아닌 card를 임의로 한 장 열고
     var x = Math.floor(Math.random() * (cards.length)); // 0부터 15 사이의 난수
@@ -81,7 +83,7 @@ function machine_second_card() {
     check_match();
 }
 
-function call_machine() {
+function call_machine() { // 기계가 첫 번째 카드 열기
     console.log("Call Machine");
     // DONE이 아닌 card를 임의로 하나 열고
     var x = Math.floor(Math.random() * (cards.length)); // 0부터 15 사이의 난수
@@ -94,7 +96,7 @@ function call_machine() {
     setTimeout(machine_second_card, 1000);
 }
 
-function check_match() {
+function check_match() { // 열린 두 장의 카드가 매치하는지 확인
     console.log("Check match");
     var open_cards = [];
     for (var i = 0; i < 16; i++) {
@@ -162,7 +164,7 @@ function check_match() {
     }
 }
 
-function card_clicked(card_id) {
+function card_clicked(card_id) { // 사용자가 카드를 클릭했음
     console.log(card_id);
     if (player == MACHINE) {
         console.log("cur_user is machine");
